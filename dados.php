5f75a8c17438c8cc668159487c6ebfe677a1f2f5
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-08-18 09:21:37 -0300

    ARRAY FEITO

---
+++ dados.php
@@ -0,0 +1,36 @@
+<?php 
+    $colecao_artigos = array(
+        array(
+            'titulo' => 'Article1',
+            'subtitulo' => 'LOREM EPSUM',
+            'Texto' => 'abadabadooooooooo',
+            'img' => 'IMG/Calling Card.png'
+        ),
+        array(
+            'titulo' => 'Article2',
+            'subtitulo' => 'LOREM EPSUM',
+            'Texto' => 'abadabadooooooooo',
+        ),
+        array(
+            'titulo' => 'Article1',
+            'subtitulo' => 'LOREM EPSUM',
+            'Texto' => 'abadabadooooooooo'
+        ),
+        array(
+          'titulo' => 'Mais Lidos',  
+          array(
+            'noticia1',
+            'noticia2',
+            'noticia3',
+            'noticia4',
+            'noticia5'
+          ),
+          array(
+            'IMG/IMG',
+            'IMG/IMG',
+            'IMG/IMG',
+            'IMG/IMG'
+          )  
+        ),      
+    );
+?>
