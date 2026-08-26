cccabf6427de7c4d9dc3d2b9fa29d70fadea955c
TulyowZ <117681814+ArthurTeixeira456@users.noreply.github.com>
2026-08-26 08:58:55 -0300

    Grid completa

--- funcao.php
+++ funcao.php
@@ -1,55 +1,5 @@
 <?php  
 
-function CriaTudo(array $conteudo) {
-    
-    for($i = 1; $i < 5; $i++) {
-        if ($i == 1) {
-            
-        echo "<section class='articles' id='article1'>
-                <div class='caixaTitulo'>
-                    <h1>".$conteudo[$i]['titulo']."</h1>
-                </div>
-                <img src='". $conteudo[$i]['img'] ."''>
-                <div class='caixaTexto'>
-                <h2> ". $conteudo[$i]['subtitulo'] ."</h2>
-                <p>". $conteudo[$i]['previa'] ."</p>
-                <a href='artigo.php?id=". $conteudo[$i]["id"] ."'>leia mais</a>
-            </section>";
-        }
-        if($i == 2 || 3 && $i > 1 && $i < 4){
-        echo "<section class='Articles'>
-                <div>
-                    <h1> ". $conteudo[$i]['titulo'] ." </h1>
-                </div>
-                <h2> ". $conteudo[$i]['subtitulo'] ."</h2>
-                <p> ". $conteudo[$i]['previa'] ."</p>
-                <a href='artigo.php?id=". $conteudo[$i]['id'] ."'>leia mais</a>
-            </section>";
-        }
-        if($i == 4) {
-            echo "<aside>
-                <input type='search'>
-                <div>
-                    <h1> ". $conteudo[$i]['titulo'] ."</h1>
-                    <ul>";
-            foreach($conteudo[$i][0] as $index => $conteudolista) {
-                echo "<li>$conteudolista</li>";
-            };
-            echo "</ul>
-                </div>
-                <div>
-                    <ul>";
-                    foreach($conteudo[$i][1] as $index => $imgs) {
-                        echo "<li>$imgs</li>";
-                    }
-            echo   "</ul>
-                </div>
-            </aside>";  
-        }
-
-}
-}
-
 function criaAside(array $conteudoaside) {
         echo "<aside>
             <input type='search'>
@@ -82,4 +32,53 @@ function criaArtigo(array $conteudoartigo) {
           </section>";
 }
 
+function CriaTudo(array $conteudo) {
+    
+    for($i = 1; $i < 5; $i++) {
+        if ($i == 1) {
+            
+        echo "<section class='articles' id='article1'>
+                <div class='caixaTitulo'>
+                    <h1>".$conteudo[$i]['titulo']."</h1>
+                </div>
+                <img src='". $conteudo[$i]['img'] ."''>
+                <div class='caixaTexto'>
+                <h2> ". $conteudo[$i]['subtitulo'] ."</h2>
+                <p>". $conteudo[$i]['previa'] ."</p>
+                <a href='artigo.php?id=". $conteudo[$i]["id"] ."'>leia mais</a>
+            </section>";
+        }
+        if($i == 2 || 3 && $i > 1 && $i < 4){
+        echo "<section class='articles'>
+                <div>
+                    <h1> ". $conteudo[$i]['titulo'] ." </h1>
+                </div>
+                <h2> ". $conteudo[$i]['subtitulo'] ."</h2>
+                <p> ". $conteudo[$i]['previa'] ."</p>
+                <a href='artigo.php?id=". $conteudo[$i]['id'] ."'>leia mais</a>
+            </section>";
+        }
+        if($i == 4) {
+            echo "<aside>
+                <input type='search'>
+                <div>
+                    <h1> ". $conteudo[$i]['titulo'] ."</h1>
+                    <ul>";
+            foreach($conteudo[$i][0] as $index => $conteudolista) {
+                echo "<li>$conteudolista</li>";
+            };
+            echo "</ul>
+                </div>
+                <div>
+                    <ul>";
+                    foreach($conteudo[$i][1] as $index => $imgs) {
+                        echo "<li>$imgs</li>";
+                    }
+            echo   "</ul>
+                </div>
+            </aside>";  
+        }
+
+    }
+}
 ?>
